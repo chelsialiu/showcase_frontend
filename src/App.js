c281a66c3e63776a7e12aea302286f8419b17577
Chelsia Liu <chelsialiu@Chelsias-MacBook-Air.local>
2021-04-10 14:13:47 -0600

    Added Quote of the Day functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import {
+  ChakraProvider,
+  Grid,
+  Box,
+  Container,
+  Flex,
+  Button,
+  Text,
+} from "@chakra-ui/react";
 
 const config = {
   apiUrl: "https://type.fit/api/quotes",
@@ -7,11 +16,14 @@ const config = {
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [quoteOfTheDay, setQuoteOfTheDay] = useState("");
+
   const Quote = ({ text, author }) => {
     return (
-      <span>
-        <strong>{text}</strong> &nbsp; <span>{author}</span>
-      </span>
+      <Box w="100%" h="100%" bg="blue.500" textAlign="center">
+        {text} <br />
+        <br /> {author}
+      </Box>
     );
   };
 
@@ -31,22 +43,46 @@ function App() {
       });
   };
 
+  const getDailyQuote = () => {
+    getQuotes();
+    if (quotes.length > 0) {
+      let randomNum = Math.floor(Math.random() * quotes.length);
+      let dailyQuote = quotes[randomNum].text;
+      setQuoteOfTheDay(dailyQuote);
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) return <Text size="xl">Loading...</Text>;
+
   return (
-    <div>
-      <button
-        onClick={() => getQuotes()}
-        type="primary"
-        disabled={isLoading}
-        size="large"
-      >
-        Fetch Quotes
-      </button>
-      <ul>
-        {quotes.map((quote) => (
-          <Quote text={quote.text} author={quote.author}></Quote>
-        ))}
-      </ul>
-    </div>
+    <ChakraProvider>
+      <Container padding="5vh" maxWidth="100vm">
+        <Flex>
+          <Button onClick={() => getDailyQuote()} disabled={isLoading}>
+            Quote of the Day
+          </Button>
+          <Box w="100%" h="100%" bg="blue.500" textAlign="center">
+            {quoteOfTheDay}
+          </Box>
+        </Flex>
+        <div>
+          <button
+            onClick={() => getQuotes()}
+            type="primary"
+            disabled={isLoading}
+            size="large"
+          >
+            Show All Quotes
+          </button>
+          <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+            {quotes.map((quote) => (
+              <Quote text={quote.text} author={quote.author}></Quote>
+            ))}
+          </Grid>
+        </div>
+      </Container>
+    </ChakraProvider>
   );
 }
 
