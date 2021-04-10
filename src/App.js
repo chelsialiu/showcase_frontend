d05cc31865fea6a9f0b132c9e961fe1a6e9683d9
Chelsia Liu <chelsialiu@Chelsias-MacBook-Air.local>
2021-04-10 16:11:24 -0600

    Added date functionality and quote formatting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   Flex,
   Button,
   Text,
+  Divider,
+  Heading,
 } from "@chakra-ui/react";
 
 const config = {
@@ -16,7 +18,8 @@ const config = {
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [quoteOfTheDay, setQuoteOfTheDay] = useState("");
+  const [quoteOfTheDay, setQuoteOfTheDay] = useState({});
+  const [today, setToday] = useState("");
 
   const Quote = ({ text, author }) => {
     return (
@@ -45,42 +48,79 @@ function App() {
 
   const getDailyQuote = () => {
     getQuotes();
+    getDate();
     if (quotes.length > 0) {
       let randomNum = Math.floor(Math.random() * quotes.length);
-      let dailyQuote = quotes[randomNum].text;
+      let dailyQuote = quotes[randomNum];
+      if (dailyQuote.author == "") {
+        dailyQuote.author = "Anonymous";
+      }
       setQuoteOfTheDay(dailyQuote);
       setIsLoading(false);
     }
+  };
+
+  const getDate = () => {
+    let currentDate = new Date();
+    let cDay = currentDate.getDate();
+    let cMonth = currentDate.getMonth() + 1;
+    let cYear = currentDate.getFullYear();
+    let today = cMonth + "/" + cDay + "/" + cYear;
+    setToday(today);
   };
 
   if (isLoading) return <Text size="xl">Loading...</Text>;
 
   return (
     <ChakraProvider>
-      <Container padding="5vh" maxWidth="100vm">
-        <Flex>
-          <Button onClick={() => getDailyQuote()} disabled={isLoading}>
-            Quote of the Day
-          </Button>
-          <Box w="100%" h="100%" bg="blue.500" textAlign="center">
-            {quoteOfTheDay}
-          </Box>
-        </Flex>
-        <div>
-          <button
-            onClick={() => getQuotes()}
-            type="primary"
-            disabled={isLoading}
-            size="large"
+      <Container maxWidth="100vm" backgroundColor="turquoise">
+        <Heading
+          as="h1"
+          size="4xl"
+          textAlign="center"
+          padding="4vh"
+          color="white"
+          backgroundColor="blue.700"
+          w="100%"
+        >
+          Quote of the Day
+        </Heading>
+        <Container minW="100vh" minH="50vh" centerContent padding="2vh">
+          <Heading as="h3" color="white" padding="3vh">
+            {today}
+          </Heading>
+          <Divider />
+          <Text
+            fontSize="4xl"
+            textAlign="center"
+            color="gray.600"
+            paddingTop="2vh"
           >
-            Show All Quotes
-          </button>
+            {quoteOfTheDay.text}
+          </Text>
+          <Text
+            fontSize="2xl"
+            textAlign="center"
+            color="gray.600"
+            paddingTop="2vh"
+            paddingBottom="2vh"
+          >
+            {quoteOfTheDay.author}
+          </Text>
+          <Button onClick={() => getDailyQuote()} disabled={isLoading}>
+            See Another Quote
+          </Button>
+        </Container>
+        {/* <Flex backgroundColor="black">
+          <Button onClick={() => getQuotes()} disabled={isLoading}>
+            All Quotes
+          </Button>
           <Grid templateColumns="repeat(5, 1fr)" gap={6}>
             {quotes.map((quote) => (
               <Quote text={quote.text} author={quote.author}></Quote>
             ))}
           </Grid>
-        </div>
+        </Flex> */}
       </Container>
     </ChakraProvider>
   );
